c1e8035024f0114f945e39dc685d3ee7a77f07ba
mtinyortaden <mtinyortaden@gmail.com>
2024-07-04 20:53:18 +0000

    week5-code

---
+++ code3/output.tf
@@ -0,0 +1,11 @@
+output "my-public-ip" {
+  value = aws_lightsail_instance.custom.public_ip_address
+}
+
+output "My_username" {
+  value = aws_lightsail_instance.custom.username
+}
+
+output "My_key" {
+  value = aws_lightsail_instance.custom.key_pair_name
+}
